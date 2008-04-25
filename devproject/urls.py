770325b83de27bf7a68a57a501636825d3f582da
James Tauber <jtauber@jtauber.com>
2008-04-25 01:21:34 +0000

    initial project/app
    git-svn-id: http://django-email-confirmation.googlecode.com/svn/trunk@2 e143efbd-a74b-0410-b764-bd10452ab0ba

---
+++ devproject/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls.defaults import *
+
+urlpatterns = patterns('',
+    # Example:
+    # (r'^devproject/', include('devproject.foo.urls')),
+
+    # Uncomment this for admin:
+#     (r'^admin/', include('django.contrib.admin.urls')),
+)

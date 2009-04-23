dd2c57a0dbc9182bbdd79f4936fbd62d8376d93f
Jannis Leidel <jannis@leidel.info>
2009-04-23 08:50:19 +0200

    Fixed bundling templates

--- setup.py
+++ setup.py
@@ -2,13 +2,19 @@ from setuptools import setup, find_packages
  
 setup(
     name='django-email-confirmation',
-    version='0.1.1',
+    version='0.1.2',
     description='Simple email confirmation for the Django web framework.',
     long_description=open('docs/index.txt').read(),
     author='James Tauber',
     author_email='jtauber@jtauber.com',
     url='http://code.google.com/p/django-email-confirmation/',
     packages=find_packages(exclude=['devproject.devtest', 'devproject']),
+    package_data = {
+        'emailconfirmation': [
+            'templates/emailconfirmation/*.txt'
+        ],
+    },
+    zip_safe=False,
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Environment :: Web Environment',

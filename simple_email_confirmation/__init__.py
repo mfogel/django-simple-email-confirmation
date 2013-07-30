b6adf00aa968288a3e3a321e2583816787a131fe
Mike Fogel <mike@fogel.ca>
2013-07-29 22:46:51 -0700

    Remove flake8 checking by travis

--- simple_email_confirmation/__init__.py
+++ simple_email_confirmation/__init__.py
@@ -1,6 +1,6 @@
 __version__ = '0.9'
 
-from .models import SimpleEmailConfirmationUserMixin, EmailAddress      # noqa
-from .signals import (                                                  # noqa
+from .models import SimpleEmailConfirmationUserMixin, EmailAddress
+from .signals import (
     email_confirmed, unconfirmed_email_created, primary_email_changed,
 )

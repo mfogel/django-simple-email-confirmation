9937deb13f5369055a2c3c338d966cc86db4f20e
Mike Fogel <mike@prismskylabs.com>
2013-08-08 11:35:04 -0700

    Update admin.py

--- simple_email_confirmation/admin.py
+++ simple_email_confirmation/admin.py
@@ -1,13 +1,9 @@
 from django.contrib import admin
 
-from .models import EmailAddress, EmailConfirmation
+from .models import EmailAddress
 
 
 class EmailAddressAdmin(admin.ModelAdmin):
-    list_display = ('__unicode__', 'user', 'is_primary')
-
-class EmailConfirmationAdmin(admin.ModelAdmin):
-    list_display = ('__unicode__', 'key', 'created_at', 'confirmed_at')
+    list_display = ('user', 'email', 'key', 'created_at', 'confirmed_at')
 
 admin.site.register((EmailAddress,), EmailAddressAdmin)
-admin.site.register((EmailConfirmation,), EmailConfirmationAdmin)

5a45617c42306c4178807422e0f2bfe4233d3c9a
Guillaume Andreu Sabater <guillaume.andreu.sabater@gmail.com>
2014-10-05 01:01:22 +0200

    Add django migrations

---
+++ simple_email_confirmation/migrations/0001_initial.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import django.utils.timezone
+from django.conf import settings
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='EmailAddress',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('email', models.EmailField(max_length=255)),
+                ('key', models.CharField(unique=True, max_length=40)),
+                ('set_at', models.DateTimeField(default=django.utils.timezone.now, help_text='When the confirmation key expiration was set')),
+                ('confirmed_at', models.DateTimeField(help_text='First time this email was confirmed', null=True, blank=True)),
+                ('user', models.ForeignKey(related_name='email_address_set', to=settings.AUTH_USER_MODEL)),
+            ],
+            options={
+                'verbose_name_plural': 'email addresses',
+            },
+            bases=(models.Model,),
+        ),
+        migrations.AlterUniqueTogether(
+            name='emailaddress',
+            unique_together=set([('user', 'email')]),
+        ),
+    ]

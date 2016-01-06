75ef5c730e5e07946b42487b83af02dd68b68bb5
Sergey Sinitsyn <sinitsynsv@yandex.ru>
2016-01-06 18:28:39 +0500

    Add django 1.9 support

--- simple_email_confirmation/__init__.py
+++ simple_email_confirmation/__init__.py
@@ -1,13 +1,11 @@
 __version__ = '0.12'
 __all__ = [
-    'SimpleEmailConfirmationUserMixin',
-    'EmailAddress',
     'email_confirmed',
     'unconfirmed_email_created',
     'primary_email_changed',
 ]
 
-from .models import SimpleEmailConfirmationUserMixin, EmailAddress
+
 from .signals import (
     email_confirmed, unconfirmed_email_created, primary_email_changed,
 )

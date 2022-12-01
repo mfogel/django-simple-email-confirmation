d2046110803e5fbd2725fe4538a37fea4974e4ad
Denis Novikov <alpden550@gmail.com>
2022-12-01 16:05:07 +0300

    feat(signals): handle signal for new django

--- simple_email_confirmation/signals.py
+++ simple_email_confirmation/signals.py
@@ -1,7 +1,10 @@
 from django.dispatch import Signal
 
-email_confirmed = Signal(providing_args=['user', 'email'])
-unconfirmed_email_created = Signal(providing_args=['user', 'email'])
-primary_email_changed = Signal(
-    providing_args=['user', 'new_email', 'old_email'],
-)
+try:
+    email_confirmed = Signal(providing_args=['user', 'email'])
+    unconfirmed_email_created = Signal(providing_args=['user', 'email'])
+    primary_email_changed = Signal(providing_args=['user', 'new_email', 'old_email'])
+except TypeError:
+    email_confirmed = Signal()
+    unconfirmed_email_created = Signal()
+    primary_email_changed = Signal()

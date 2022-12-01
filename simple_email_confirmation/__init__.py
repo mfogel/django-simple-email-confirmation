1afd860713e708367770206d004195b1e50b2243
Denis Novikov <alpden550@gmail.com>
2022-12-01 16:15:22 +0300

    feat(init): return get_email_address_model

--- simple_email_confirmation/__init__.py
+++ simple_email_confirmation/__init__.py
@@ -8,6 +8,8 @@ __all__ = [
 ]
 
 import django
+from django.apps import apps
+from django.conf import settings
 
 from .models import SimpleEmailConfirmationUserMixin, EmailAddress
 from .signals import (
@@ -16,3 +18,12 @@ from .signals import (
 
 if django.get_version() < '4':
     default_app_config = 'simple_email_confirmation.apps.EmailAddress'
+
+
+def get_email_address_model():
+    """Convenience method to return the email model being used."""
+    return apps.get_model(getattr(
+        settings,
+        'SIMPLE_EMAIL_CONFIRMATION_EMAIL_ADDRESS_MODEL',
+        'simple_email_confirmation.EmailAddress'
+    ))
